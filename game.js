1c434be79618146ed51665cab85b66912052f92b
Brian Clark <bclark8923@gmail.com>
2015-04-28 09:42:15 -0700

    added lesson points

--- game.js
+++ game.js
@@ -6,12 +6,12 @@ function makePipePair(group, offsetX, newOffsetX) {
     var top = group.create(0, 0, 'pipe_top');
     var bottom = group.create(0, 0, 'pipe_bottom');
     top.anchor.set(0, 1);
-    var spacing = 100;
+    var spacing = 100; // LESSON: vertical spacing between pipes: 50/200
     function addPhysics(pipe) {
         game.physics.enable(pipe);
         pipe.body.immovable = true;
         pipe.body.allowGravity = false;
-        pipe.body.velocity.x = -200;
+        pipe.body.velocity.x = -200; // LESSON: change speed pipes come at you: -500
     }
     function positionPipes(top, bottom) {
         var center = game.rnd.integerInRange(50, game.world.height - 50);
@@ -46,12 +46,16 @@ var mainState = {
         game.load.image('floor', 'images/floor.png');
         game.load.image('pipe_top', 'images/pipe_top.png');
         game.load.image('pipe_bottom', 'images/pipe_bottom.png');
+        // game.load.image('sky', 'images/back.png');
     },
     create: function () {
         // This function is called after the preload function
         // Here we set up the game, display sprites, etc.
         this.floor = game.add.tileSprite(0, game.world.height - 40, game.world.width, game.world.height, 'floor');
         this.floor.tileScale.set(0.5);
+
+        //  A simple background for our game
+        // game.add.sprite(0, -450, 'sky');
 
         this.obstacles = game.add.group();
         this.obstacles.add(this.floor);
@@ -68,7 +72,7 @@ var mainState = {
         this.sprite.animations.add('flap', [0,1,2,1], 10, true);
         this.sprite.animations.play('flap');
         game.physics.enable(this.sprite);
-        game.physics.arcade.gravity.y = 250;
+        game.physics.arcade.gravity.y = 250; // LESSON: change gravity: 950
         // Stop the bird from falling off the screen, for now
         this.sprite.body.collideWorldBounds = true;
         // Change background color to a gray color
@@ -76,7 +80,7 @@ var mainState = {
         // keep space from scrolling the page
         this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
-        var pipeSpacing = 400;
+        var pipeSpacing = 400; // LESSON: distance betweent pipes horizontally: 1000/200
         var numPipes = 4;
         var i;
         for (i = 0; i < numPipes; i += 1) {
@@ -87,7 +91,7 @@ var mainState = {
         // This function is called 60 times per second
         // It contains the game's logic
         if (this.spaceKey.justDown) {
-            this.sprite.body.velocity.y = -150;
+            this.sprite.body.velocity.y = -150; // LESSON: change jump speed: -350
         }
         this.floor.tilePosition.x -= 15;
         if (game.physics.arcade.collide(this.sprite, this.obstacles)) {
